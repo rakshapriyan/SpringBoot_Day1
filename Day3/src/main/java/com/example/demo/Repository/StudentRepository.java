98a28017f3b40b1a8bc3b5aa140dfe0129ebb96e
rakshapriyan <98371140+rakshapriyan@users.noreply.github.com>
2023-02-23 18:02:07 +0530

    Initial

--- Day3/src/main/java/com/example/demo/Repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-}
